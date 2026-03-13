f04a5e020cb355ac866a6d750edc525a4d451a98
Mohamed Abdi <mosolves147@gmail.com>
2026-03-13 17:34:31 +0300

    Defining common data types

--- GameEngine/include/framework/Core.h
+++ GameEngine/include/framework/Core.h
@@ -1,7 +1,32 @@
 #pragma once
 #include <stdio.h>
+#include <memory>
+#include <map>
+#include <unordered_map>
 
 namespace gdev {
+// Renaming data types
+template<typename T>
+using unique = std::unique_ptr<T>;
+
+template<typename T>
+using shared = std::shared_ptr<T>;
+
+template<typename T>
+using weak = std::weak_ptr<T>;
+
+template<typename T>
+using List = std::vector<T>;
+
+// this is orders map based on keys using std::less with Pr
+template<typename keyType, typename valType, typename Pr = std::less<keyType>>
+using Map = std::map<keyType, valType, Pr>;
+
+// this is unordered map
+template<typename keyType, typename valType, typename hasher = std::hash<keyType>>
+using Dictionary = std::unordered_map<keyType, valType, hasher>;
+
+// Macro
 #define LOG(M, ...) printf(M "\n", ##__VA_ARGS__)
 
 }
